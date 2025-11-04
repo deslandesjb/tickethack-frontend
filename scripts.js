140a40f37a946ca6a7d3d2d8ff4dc0a54ddb0f61
Jb <deslandes.jb@gmail.com>
2025-11-04 15:03:41 +0100

    script added with fetch logic based on backend

---
+++ scripts.js
@@ -0,0 +1,23 @@
+document.querySelector("#todo").addEventListener("click", function () {
+  const cityDeparture = document.querySelector("#todo");
+  const cityArrival = document.querySelector("#todo");
+  const cityDate = document.querySelector("#todo");
+
+  fetch("http://localhost:3000/trips", {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({departure: cityDeparture.value}, {arrival: cityArrival.value}, {date: cityDate.value}),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.result) {
+        // document.querySelector("#cityList").innerHTML += `
+
+        // `;
+
+        cityDeparture.value = "";
+        cityArrival.value = "";
+        cityDate.value = "";
+      }
+    });
+});
